cd75490959bcf41c14190cca871c758a0677818b
Konstantin Pukhkiy <konstantin.os.1024@gmail.com>
2024-12-03 20:53:01 +0300

    add docxbuilder

--- auto_docs_en/conf.py
+++ auto_docs_en/conf.py
@@ -20,7 +20,8 @@ release = '1.0.0'
 extensions = [
 	'sphinx.ext.autodoc',
 	'sphinx.ext.viewcode',
-	'sphinx.ext.napoleon'
+	'sphinx.ext.napoleon',
+	'docxbuilder'
 ]
 
 templates_path = ['_templates']
